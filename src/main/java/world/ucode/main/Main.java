b8ef7a840e205e3cf6c07251506c03d9a5332d78
vstruk01 <stoliar444@gmail.com>
2020-10-01 13:19:18 +0300

    include javafx

--- src/main/java/world/ucode/main/Main.java
+++ src/main/java/world/ucode/main/Main.java
@@ -1,7 +1,23 @@
 package world.ucode.main;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+
+public class Main extends Application {
+
+    public void start(Stage primaryStage) {
+        Group g = new Group();
+        Scene scene = new Scene(g,1000, 1000);
+
+        primaryStage.setTitle("Hello World !");
+        primaryStage.setScene(scene);
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World !");
+        launch(args);
     }
 }
